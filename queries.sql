2e6dd7b239aed31da48da0712b7e74fcab6c2232
Fallass <158059534+Fallass@users.noreply.github.com>
2024-08-27 13:50:22 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 select count(customer_id) as customers_count 
 from customers c 
-(считает общее количество покупателей из таблицы customers)
+
 
 
 select concat(e.first_name,' ', e.last_name) as seller,
@@ -14,9 +14,6 @@ on p.product_id = s.product_id
 group by concat(e.first_name,' ', e.last_name)
 order by  income desc
 limit 10
-(Таблица состоит из трех колонок - данных о продавце,
- суммарной выручке с проданных товаров и количестве проведенных сделок,
-  и отсортирована по убыванию выручки)
 
 
 with tab as (
@@ -37,7 +34,7 @@ on s.sales_person_id = e.employee_id
 left join products p 
 on p.product_id = s.product_id)
 order by average_income
-(продавцы, чья средняя выручка за сделку меньше средней выручки за сделку по всем продавцам)
+
 
 select concat(e.first_name,' ', e.last_name) as seller,
 lower(to_char(s.sale_date, 'DAY')) as day_of_week ,
@@ -49,8 +46,7 @@ left join products p
 on p.product_id = s.product_id
 group by concat(e.first_name,' ', e.last_name), to_char(s.sale_date, 'DAY'), to_char(s.sale_date,'id')
 order by to_char(s.sale_date,'id'), seller
-(Третий отчет содержит информацию о выручке по дням недели. Каждая запись содержит имя и фамилию продавца,
- день недели и суммарную выручку)
+
  
  select case
 	when age between 16 and 25 then '16-25'
@@ -62,7 +58,7 @@ from customers c
 where age is not null
 group by age_category
 order by min(age)
-(количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+)
+
 
 select to_char(sale_date, 'YYYY-MM') AS selling_month,
 count(distinct(s.customer_id)) as total_customers,
@@ -74,7 +70,7 @@ left join products p
 on p.product_id = s.product_id
 group by to_char(sale_date, 'YYYY-MM')
 order by selling_month ASC
-( данные по количеству уникальных покупателей и выручке, которую они принесли)
+
 
 select concat(c.first_name,' ', c.last_name) as customer,
 min(sale_date) as sale_date,
@@ -91,4 +87,4 @@ group by concat(c.first_name,' ', c.last_name), concat(e.first_name,' ', e.last_
 HAVING 
     MIN(sale_date) IS NOT null
 order by customer
-(покупатели, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0))
+
