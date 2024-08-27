d4a0a5b737b2536516766bbfa92a1224c91908ba
Fallass <158059534+Fallass@users.noreply.github.com>
2024-08-27 14:40:32 +0300

    queries.sql

--- queries.sql
+++ queries.sql
@@ -2,15 +2,13 @@ select count(customer_id) as customers_count
 from customers
 
 
-select e.first_name||' '||e.last_name as seller,
-count(s.sales_id) as operations,
+select count(s.sales_id) as operations,
 round(sum(quantity * price),0) as income
 from sales s
 left join employees e
 on s.sales_person_id = e.employee_id
 left join products p 
 on p.product_id = s.product_id 
-group by e.first_name||' '||e.last_name
 order by  income desc
 limit 10
 
